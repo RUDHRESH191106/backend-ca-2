b9f925f0fef91aec3955fb26578fc8b4cbb30a44
RUDHRESH M <rudhreshm191106@gmail.com>
2025-02-24 14:24:11 +0530

    Done

---
+++ server.js
@@ -0,0 +1,41 @@
+
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+let books = [];
+
+app.get("/", (req, res) => {
+    res.send("Library API is running!");
+});
+
+app.post("/books", (req, res) => {
+    books.push(req.body);
+    res.json({ message: "Book added!" });
+});
+
+app.get("/books", (req, res) => {
+    res.json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+    const book = books.find(b => b.book_id == req.params.id);
+    res.json(book || { error: "Book not found" });
+});
+
+app.put("/books/:id", (req, res) => {
+    const book = books.find(b => b.book_id == req.params.id);
+    if (!book) return res.status(404).json({ error: "Book not found" });
+
+    Object.assign(book, req.body);
+    res.json({ message: "Book updated!", book });
+});
+
+app.delete("/books/:id", (req, res) => {
+    books = books.filter(b => b.book_id != req.params.id);
+    res.json({ message: "Book deleted!" });
+});
+app.listen(port, () => {
+    console.log(`Library API running at http://localhost:${port}`);
+});
